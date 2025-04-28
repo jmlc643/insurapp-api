265383d0f4ba247f17323d60d67bd571deefc4ed
jmlc643 <josemarialuyocampos@gmail.com>
2025-04-27 23:22:03 -0500

    First commit

---
+++ src/main/java/com/upao/insurApp/InsurAppApplication.java
@@ -0,0 +1,13 @@
+package com.upao.insurApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsurAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InsurAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/upao/insurApp/InsurAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.upao.insurApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InsurAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
