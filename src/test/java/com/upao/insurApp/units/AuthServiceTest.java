6e43cbedb81543de00373e81a7180455e970c984
jmlc643 <josemarialuyocampos@gmail.com>
2025-06-15 23:23:30 -0500

    test(Backend): Se añadieron tests de login

--- src/test/java/com/upao/insurApp/units/AuthServiceTest.java
+++ src/test/java/com/upao/insurApp/units/AuthServiceTest.java
@@ -1,5 +1,10 @@
 package com.upao.insurApp.units;
 
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.Claim;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import com.upao.insurApp.dto.auth.AuthRequestDTO;
+import com.upao.insurApp.dto.auth.AuthResponseDTO;
 import com.upao.insurApp.dto.email.Mail;
 import com.upao.insurApp.dto.user.RegisterUserRequest;
 import com.upao.insurApp.exceptions.ResourceAlreadyExistsException;
@@ -14,26 +19,28 @@ import com.upao.insurApp.services.AuthService;
 import com.upao.insurApp.services.EmailService;
 import com.upao.insurApp.services.JwtDetailsService;
 import com.upao.insurApp.utils.JwtUtils;
+import com.upao.insurApp.utils.UserSecurity;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.thymeleaf.spring6.SpringTemplateEngine;
 
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Optional;
+import java.util.*;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.*;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 public class AuthServiceTest {
@@ -93,5 +100,52 @@ public class AuthServiceTest {
 
         // Then
         assertThat(ex.getMessage()).isEqualTo("El email ya ha sido usado para la creación de otro usuario");
+    }
+
+    @Test
+    void testLogin() {
+        // Given
+        AuthRequestDTO request = new AuthRequestDTO("email@example.com", "password");
+        User user = new User(1, "Jose", "Luyo", "12345678", "123456789", "email@example.com", "password", ERole.USER);
+        UserDetails userDetails = new UserSecurity(1, "password", Collections.singleton(new SimpleGrantedAuthority("ROLE_USER")), user);
+        Code code = new Code(1, "123456", TypeCode.ACTIVATE, EStatus.VERIFIED, LocalDateTime.now().plusMinutes(10), user);
+        DecodedJWT decodedJWT = mock(DecodedJWT.class);
+
+        // When
+        when(jwtDetailsService.loadUserByUsername("email@example.com")).thenReturn(userDetails);
+        when(passwordEncoder.matches("password", userDetails.getPassword())).thenReturn(true);
+        when(userRepository.findById(anyInt())).thenReturn(Optional.of(user));
+        when(codeRepository.findFirstByUser(any(User.class))).thenReturn(Optional.of(code));
+        when(jwtUtils.generateToken(any(Authentication.class))).thenReturn("accessToken");
+        when(jwtUtils.validateJWT("accessToken")).thenReturn(decodedJWT);
+
+        AuthResponseDTO result = authService.login(request);
+
+        // Assert
+        Assertions.assertThat(result).isNotNull();
+        assertThat(result.getRole()).isEqualTo("ROLE_USER");
+        Assertions.assertThat(result.getToken()).isEqualTo("accessToken");
+    }
+
+    @Test
+    void testLoginWhenJwtIsNotValid() {
+        // Given
+        AuthRequestDTO request = new AuthRequestDTO("email@example.com", "password");
+        User user = new User(1, "Jose", "Luyo", "12345678", "123456789", "email@example.com", "password", ERole.USER);
+        UserDetails userDetails = new UserSecurity(1, "password", Collections.singleton(new SimpleGrantedAuthority("ROLE_USER")), user);
+        Code code = new Code(1, "123456", TypeCode.ACTIVATE, EStatus.VERIFIED, LocalDateTime.now().plusMinutes(10), user);
+
+        // When
+        when(jwtDetailsService.loadUserByUsername("email@example.com")).thenReturn(userDetails);
+        when(passwordEncoder.matches("password", userDetails.getPassword())).thenReturn(true);
+        when(userRepository.findById(anyInt())).thenReturn(Optional.of(user));
+        when(codeRepository.findFirstByUser(any(User.class))).thenReturn(Optional.of(code));
+        when(jwtUtils.generateToken(any(Authentication.class))).thenReturn("accessToken");
+        doThrow(new JWTVerificationException("Token inválido, no estás autorizado")).when(jwtUtils).validateJWT(anyString());
+
+        JWTVerificationException ex = assertThrows(JWTVerificationException.class, () -> authService.login(request));
+
+        // Then
+        Assertions.assertThat(ex.getMessage()).isEqualTo("Token inválido, no estás autorizado");
     }
 }
